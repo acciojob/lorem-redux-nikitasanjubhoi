6753beb66ab29fc1f93b91d942b496587c895f7c
Nikita bhoi <114177037+nikitasanjubhoi@users.noreply.github.com>
2025-11-16 12:04:25 +0530

    Update Lorem.js

--- src/components/Lorem.js
+++ src/components/Lorem.js
@@ -1,1 +1,25 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchLorem } from "../store/actions";
+
+const Lorem = () => {
+  const dispatch = useDispatch();
+  const { loading, data, error } = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch(fetchLorem());
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <p>
+      <strong>{data?.title}</strong><br />
+      {data?.body}
+    </p>
+  );
+};
+
+export default Lorem;
 
