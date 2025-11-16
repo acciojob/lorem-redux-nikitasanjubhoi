990834bc8bb3f81b599902780aafc67a5c0767aa
Nikita bhoi <114177037+nikitasanjubhoi@users.noreply.github.com>
2025-11-16 12:00:48 +0530

    Update reducer.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,1 +1,28 @@
+import {
+    FETCH_LOREM_REQUEST,
+    FETCH_LOREM_SUCCESS,
+    FETCH_LOREM_FAILURE
+} from "./actions";
+
+const initialState = {
+    loading: false,
+    data: null,
+    error: null
+};
+
+export const loremReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_LOREM_REQUEST:
+            return { ...state, loading: true };
+
+        case FETCH_LOREM_SUCCESS:
+            return { ...state, loading: false, data: action.payload };
+
+        case FETCH_LOREM_FAILURE:
+            return { ...state, loading: false, error: action.payload };
+
+        default:
+            return state;
+    }
+};
 
