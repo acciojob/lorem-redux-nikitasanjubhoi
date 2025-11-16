166bde363fb0ee2ae6f78b9fde47689210452ff7
Nikita bhoi <114177037+nikitasanjubhoi@users.noreply.github.com>
2025-11-16 11:45:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 
 import React from "react";
 import './../styles/App.css';
+import Lorem from "./Lorem";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <Lorem />
     </div>
   )
 }
 
-export default App
+export default App;
