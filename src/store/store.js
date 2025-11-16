47a194adacff6daa7935b4c79d4381952afff926
Nikita bhoi <114177037+nikitasanjubhoi@users.noreply.github.com>
2025-11-16 12:01:58 +0530

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,1 +1,6 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { loremReducer } from "./reducer";
+
+export const store = createStore(loremReducer, applyMiddleware(thunk));
 
